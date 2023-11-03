fbeeb5fc50e245701dddf6c2f79205606d568f21
Borysov <alex72@te.net.ua>
2023-11-04 01:48:18 +0200

    Homework4 - Figure

---
+++ src/Figure.java
@@ -0,0 +1,106 @@
+/* "Choose figure what you want:
+    1 - Rectangle;
+    2 - RightTriangle;
+    3 - InvertedTriangle;
+    4 - NormalTriangle"
+*/
+
+import java.util.Scanner;
+
+public class Figure {
+    public static void main(String[] args) {
+        String proceed = "yes";
+        while (proceed.equals("yes")) {
+            System.out.println("Choose figure what you want:\n" +
+                    "    1 - Rectangle;\n" +
+                    "    2 - RightTriangle;\n" +
+                    "    3 - InvertedTriangle;\n" +
+                    "    4 - NormalTriangle");
+            Scanner scanner = new Scanner(System.in);
+            int userAnswer = scanner.nextInt();
+            chooseFigure(userAnswer);
+            System.out.println("Do you want create new figure? yes - no");
+           proceed = scanner.next();
+
+        }
+
+    }
+
+    public static void chooseFigure(int figureType){
+            switch (figureType) {
+                case 1:
+                    System.out.println("Rectangle");
+                    createRectangle();
+                    break;
+                case 2:
+                    System.out.println("RightTriangle");
+                    createRightTriangle();
+                    break;
+                case 3:
+                    System.out.println("InvertedTriangle");
+                    createInvertedTriangle();
+                    break;
+                case 4:
+                    System.out.println("NormalTriangle");
+                    createNormalTriangle();
+                    break;
+            }
+
+
+    }
+    public static void createRectangle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input width");
+        int width = scanner.nextInt();
+        System.out.println("Input height");
+        int height = scanner.nextInt();
+        for(int i=0; i<height; i++){
+            for(int j=0; j<width; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void createRightTriangle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input height");
+        int height = scanner.nextInt();
+        for (int i = height; i >= 0; i--) {
+            for (int k = i; k <= height - 1; k++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    public static void createInvertedTriangle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input height");
+        int height = scanner.nextInt();
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void createNormalTriangle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input height");
+        int height = scanner.nextInt();
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i - 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
