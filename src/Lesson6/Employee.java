61bb3f0b7bee977c3dd47ceae36a1d736beebdad
Borysov <alex72@te.net.ua>
2023-11-07 00:28:20 +0200

    Product

--- src/Lesson6/Employee.java
+++ src/Lesson6/Employee.java
@@ -1,4 +1,22 @@
 package Lesson6;
 
 public class Employee {
+    private String name;
+    private String lastName;
+    private String position;
+    private int salary;
+
+    public Employee(String name, String lastName, String position, int salary) {
+        this.name = name;
+        this.lastName = lastName;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setSalary(int newSalary){
+        salary = newSalary;
+    }
 }
--- src/Lesson6/Product.java
+++ src/Lesson6/Product.java
@@ -1,11 +1,11 @@
 package Lesson6;
 
 public class Product {
-    String name;
-    int article;
-    double price;
-    String description;
-    String available;
+   private String name;
+    private int article;
+    private double price;
+    private String description;
+    private String available;
 
     public Product(String name, int article, double price, String description, String available) {
         this.name = name;
@@ -23,11 +23,21 @@ public class Product {
     public void setPrice(double newPrice){
         price = newPrice;
     }
-    public void setDescription(String newArticle){
-        article = newArticle;
+    public void setDescription(String newDescription){
+        description = newDescription;
     }
     public void setAvailable(String newAvailable){
         available = newAvailable;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getAvailable(){
+        return this.available;
+    }
+
+
 
 }
