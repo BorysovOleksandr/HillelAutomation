c72ce0078ba701eafe088dde206be0963cdbec6a
Borysov <alex72@te.net.ua>
2023-11-08 02:42:35 +0200

    Part1

---
+++ src/Lesson7/Quadrangle/Square.java
@@ -0,0 +1,12 @@
+package Lesson7.Quadrangle;
+
+import Lesson7.Quadrangle.Quadrangle;
+
+public class Square extends Quadrangle {
+    private int getSideA() {
+        return super.sideA;
+    }
+    public Square(int sideA) {
+        super( int sideA, int sideB, int sideC, int sideD);
+    }
+}
